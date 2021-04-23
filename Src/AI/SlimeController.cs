f0d652d4a2c89c008ed9b001ef14fc56b25581ef
ThePythonGuy <45978337+ThePythonGuy3@users.noreply.github.com>
2021-04-23 17:58:56 +0200

    More stuff

--- Src/AI/SlimeController.cs
+++ Src/AI/SlimeController.cs
@@ -5,11 +5,11 @@ using Content;
 
 public class SlimeController : MonoBehaviour
 {
-	public Sprite sprite;
 	public Transform player;
 	public float speed;
 	public float moveLength = 240f;
 	public float stopTime = 240f;
+	SpriteRenderer sprr;
 	Rigidbody2D rb;
 	float moveTime = 0f;
 	Vector3 playerPos;
@@ -19,6 +19,7 @@ public class SlimeController : MonoBehaviour
     void Start()
     {
 		rb = GetComponent<Rigidbody2D>();
+		sprr = GetComponent<SpriteRenderer>();
     }
 
     void Update()
@@ -29,15 +30,16 @@ public class SlimeController : MonoBehaviour
 			origPos = rb.position;
 			if(moveTime >= stopTime){
 				attacking = true;
+				sprr.flipX = origPos.x > playerPos.x;
 			}
 		} else {
-			
 			rb.position = Vector3.MoveTowards(rb.position, playerPos, speed);
-			transform.rotation = Quaternion.Euler(0f, 0f, 0f);
 			if(Vector3.Distance(origPos, rb.position) >= moveLength || Vector3.Distance(playerPos, rb.position) <= 3f){
 				moveTime = 0f;
 				attacking = false;
 			}
 		}
+
+		transform.rotation = Quaternion.Euler(0f, 0f, 0f);
     }
 }
