82bb6811b474341dc41778d9bd8afe9bdc433d7d
ThePythonGuy <45978337+ThePythonGuy3@users.noreply.github.com>
2021-04-23 13:55:06 +0200

    slime movement WIP

--- Src/EnemyController.cs
+++ Src/EnemyController.cs
@@ -8,6 +8,12 @@ public class EnemyController : MonoBehaviour
 	public Sprite sprite;
 	public Transform player;
 	public float speed;
+	public float moveLength = 240f;
+	public float stopLength = 240f;
+	float movePos = 0f;
+	Vector3 playerPos;
+	Vector3 origPos;
+	bool attacking = false;
 
     void Start()
     {
@@ -15,6 +21,18 @@ public class EnemyController : MonoBehaviour
 
     void Update()
     {
-		transform.position = Vector3.MoveTowards(transform.position, player.position, speed);
+		if(!attacking){
+			movePos += 1f;
+			playerPos = player.position;
+			origPos = transform.position;
+			if(movePos >= moveLength) attacking = true;
+		} else {
+			transform.position = Vector3.MoveTowards(transform.position, playerPos, speed);
+			movePos -= 1;
+			if(movePos <= moveLength - stopLength){
+				movePos = 0f;
+				attacking = false;
+			}
+		}
     }
 }
