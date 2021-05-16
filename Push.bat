c7f16074b1f40f58608d4f077807b2253ea8454c
ThePythonGuy <45978337+ThePythonGuy3@users.noreply.github.com>
2021-05-16 18:15:55 +0200

    Bat finished (ditched python commiter)

--- Push.bat
+++ Push.bat
@@ -1,14 +1,19 @@
 @echo off
+
 set /p id=Commit Name: 
+
 set repos=.gitpublic .gitprivate
+
 (for %%a in (%repos%) do ( 
 	echo Adding %%a
 	git --git-dir=%%a add .
-	echo Added %%a
+	echo Added %%a && .
+
 	echo Commiting %%a
 	git --git-dir=%%a commit -m "%id%"
-	echo Commited %%a
+	echo Commited %%a && .
+
 	echo Pushing %%a
 	git --git-dir=%%a push
-	echo Pushed %%a
+	echo Pushed %%a && .
 ))
